31438ee4d447607334bc215c4b0d7e1074df1f9c
Fraser <28698343+FrazzIe@users.noreply.github.com>
2018-05-26 13:46:29 +0100

    Add files via upload

---
+++ RideADeer/client.lua
@@ -0,0 +1,228 @@
+--entityEnumerator Written by IllidanS4
+--[[
+The MIT License (MIT)
+Copyright (c) 2017 IllidanS4
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+]]
+local entityEnumerator = {
+	__gc = function(enum)
+		if enum.destructor and enum.handle then
+			enum.destructor(enum.handle)
+		end
+		enum.destructor = nil
+		enum.handle = nil
+	end
+}
+
+function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+	return coroutine.wrap(function()
+		local iter, id = initFunc()
+		if not id or id == 0 then
+			disposeFunc(iter)
+			return
+		end
+	
+		local enum = {handle = iter, destructor = disposeFunc}
+		setmetatable(enum, entityEnumerator)
+	
+		local next = true
+		repeat
+			coroutine.yield(id)
+			next, id = moveFunc(iter)
+		until not next
+	
+		enum.destructor, enum.handle = nil, nil
+		disposeFunc(iter)
+	end)
+end
+
+function EnumeratePeds()
+    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+--]]
+
+local Deer = {
+	Handle = nil,
+	Invincible = false,
+	Ragdoll = false,
+	Marker = false,
+	Speed = {
+		Walk = 2.0,
+		Run = 3.0,
+	},
+}
+
+function GetNearbyPeds(X, Y, Z, Radius)
+	local NearbyPeds = {}
+	for Ped in EnumeratePeds() do
+		if DoesEntityExist(Ped) then
+			local PedPosition = GetEntityCoords(Ped, false)
+			if Vdist(X, Y, Z, PedPosition.x, PedPosition.y, PedPosition.z) <= Radius then
+				table.insert(NearbyPeds, Ped)
+			end
+		end
+	end
+	return NearbyPeds
+end
+
+function GetCoordsInfrontOfEntityWithDistance(Entity, Distance, Heading)
+	local Coordinates = GetEntityCoords(Entity, false)
+	local Head = (GetEntityHeading(Entity) + (Heading or 0.0)) * math.pi / 180.0
+	return {x = Coordinates.x + Distance * math.sin(-1.0 * Head), y = Coordinates.y + Distance * math.cos(-1.0 * Head), z = Coordinates.z}
+end
+
+function GetGroundZ(X, Y, Z)
+	if tonumber(X) and tonumber(Y) and tonumber(Z) then
+		local _, GroundZ = GetGroundZFor_3dCoord(X + 0.0, Y + 0.0, Z + 0.0, Citizen.ReturnResultAnyway())
+		return GroundZ
+	else
+		return 0.0
+	end
+end
+
+function Deer.Destroy()
+	local Ped = PlayerPedId()
+
+	DetachEntity(Ped, true, false)
+	ClearPedTasksImmediately(Ped)
+
+	SetEntityAsNoLongerNeeded(Deer.Handle)
+	DeletePed(Deer.Handle)
+
+	if DoesEntityExist(Deer.Handle) then
+		SetEntityCoords(Deer.Handle, 601.28948974609, -4396.9853515625, 384.98565673828)
+	end
+
+	Deer.Handle = nil
+end
+
+function Deer.Create()
+	local Model = GetHashKey("a_c_deer")
+	RequestModel(Model)
+	while not HasModelLoaded(Model) do
+		Citizen.Wait(50)
+	end
+
+	local Ped = PlayerPedId()
+	local PedPosition = GetEntityCoords(Ped, false)
+
+	Deer.Handle = CreatePed(28, Model, PedPosition.x, PedPosition.y, PedPosition.z, GetEntityHeading(Ped), true, false)
+
+	SetPedCanRagdoll(Deer.Handle, Deer.Ragdoll)
+	SetEntityInvincible(Deer.Handle, Deer.Invincible)
+
+	SetModelAsNoLongerNeeded(Model)
+end
+
+function Deer.Attach()
+	local Ped = PlayerPedId()
+
+	FreezeEntityPosition(Deer.Handle, true)
+	FreezeEntityPosition(Ped, true)
+
+	local DeerPosition = GetEntityCoords(Deer.Handle, false)
+	SetEntityCoords(Ped, DeerPosition.x, DeerPosition.y, DeerPosition.z)
+
+	AttachEntityToEntity(Ped, Deer.Handle, GetPedBoneIndex(Deer.Handle, 24816), -0.3, 0.0, 0.3, 0.0, 0.0, 90.0, false, false, false, true, 2, true)
+
+	TaskPlayAnim(Ped, "rcmjosh2", "josh_sitting_loop", 8.0, 1, -1, 2, 1.0, 0, 0, 0)
+
+	FreezeEntityPosition(Deer.Handle, false)
+	FreezeEntityPosition(Ped, false)
+end
+
+function Deer.Ride()
+	local Ped = PlayerPedId()
+	local PedPosition = GetEntityCoords(Ped, false)
+	if IsPedSittingInAnyVehicle(Ped) or IsPedGettingIntoAVehicle(Ped) then
+		return
+	end
+
+	local AttachedEntity = GetEntityAttachedTo(Ped)
+
+	if IsEntityAttached(Ped) and GetEntityModel(AttachedEntity) == GetHashKey("a_c_deer") then
+		local SideCoordinates = GetCoordsInfrontOfEntityWithDistance(AttachedEntity, 1.0, 90.0)
+		local SideHeading = GetEntityHeading(AttachedEntity)
+
+		SideCoordinates.z = GetGroundZ(SideCoordinates.x, SideCoordinates.y, SideCoordinates.z)
+
+		Deer.Handle = nil
+		DetachEntity(Ped, true, false)
+		ClearPedTasksImmediately(Ped)
+
+		SetEntityCoords(Ped, SideCoordinates.x, SideCoordinates.y, SideCoordinates.z)
+		SetEntityHeading(Ped, SideHeading)
+	else
+		for _, Ped in pairs(GetNearbyPeds(PedPosition.x, PedPosition.y, PedPosition.z, 2.0)) do
+			if GetEntityModel(Ped) == GetHashKey("a_c_deer") then
+				Deer.Handle = Ped
+				Deer.Attach()
+				break
+			end
+		end
+	end
+end
+
+Citizen.CreateThread(function()
+	RequestAnimDict("rcmjosh2")
+	while not HasAnimDictLoaded("rcmjosh2") do
+		Citizen.Wait(250)
+	end
+	while true do
+		Citizen.Wait(0)
+
+		if IsControlJustPressed(1, 51) then
+			Deer.Ride()
+		end
+
+		if IsControlJustPressed(1, 288) then
+			if not Deer.Handle then
+				Deer.Create()
+			else
+				Deer.Destroy()
+			end
+		end
+
+		local Ped = PlayerPedId()
+		local AttachedEntity = GetEntityAttachedTo(Ped)
+
+		if (not IsPedSittingInAnyVehicle(Ped) or not IsPedGettingIntoAVehicle(Ped)) and IsEntityAttached(Ped) and AttachedEntity == Deer.Handle then
+			if DoesEntityExist(Deer.Handle) then
+				local LeftAxisXNormal, LeftAxisYNormal = GetControlNormal(2, 218), GetControlNormal(2, 219)
+				local Speed, Range = Deer.Speed.Walk, 4.0
+
+				if IsControlPressed(0, 21) then
+					Speed = Deer.Speed.Run
+					Range = 8.0
+				end
+
+				local GoToOffset = GetOffsetFromEntityInWorldCoords(Deer.Handle, LeftAxisXNormal * Range, LeftAxisYNormal * -1.0 * Range, 0.0)
+
+				TaskLookAtCoord(Deer.Handle, GoToOffset.x, GoToOffset.y, GoToOffset.z, 0, 0, 2)
+				TaskGoStraightToCoord(Deer.Handle, GoToOffset.x, GoToOffset.y, GoToOffset.z, Speed, 20000, 40000.0, 0.5)
+
+				if Deer.Marker then
+					DrawMarker(6, GoToOffset.x, GoToOffset.y, GoToOffset.z, 0, 0, 0, 0, 0, 0, 1.0, 1.0, 1.0, 255, 255, 255, 255, 0, 0, 2, 0, 0, 0, 0)
+				end
+			end
+		end
+	end
+end)
